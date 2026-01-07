dfe18efcbd5518e151334cfc4be86442b743d9d5
bhumKiran <kiran.khatri.787@gmail.com>
2026-01-07 21:24:18 +0545

    projectideas

---
+++ kirana-pasal/data/products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sampledata = () => {
+  return (
+    <div>Sample data dummydata</div>
+  )
+}
+
+export default Sampledata 
---
+++ kirana-pasal/products/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>All product details </div>
+  )
+}
+
+export default ProductDetails
