67e938c47290adff96d36b4246aca9307cadaa4a
bhumKiran <kiran.khatri.787@gmail.com>
2026-01-10 10:43:53 +0545

    appended customer with header

--- kirana-pasal/app/dashboard/customer/page.tsx
+++ kirana-pasal/app/dashboard/customer/page.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import axios from 'axios'
-const Home = async () => {
-    const {data} = await axios.get('https://api.escuelajs.co/api/v1/products')
+import Header from "@/components/header/page";
+import axios from "axios";
+
+const Home = async (children) => {
+  const { data } = await axios.get("https://api.escuelajs.co/api/v1/products");
 
   return (
     <div>
-      {data.map((item,id)=>{
-        return (
-          <div key={id} className='bg-pink-200 m-4 shadow-lg p-4 rounded w-[20%]' >
-            {item}
-         
-            </div>
-        )
-      })}
-    </div>
-  )
-}
+      <Header/>
+      <div className="grid grid-cols-5 gap-y-4 gap-x-4 items-center justify-around h-auto w-auto bg-gray-200  ml-50 mt-20 ">
+        {data.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="items-center bg-green-200 border-2 rounded shadow-2xl"
+            >
+              <h2>{item.title}</h2>
 
-export default Home
+              <img
+                className="rounded shadow-xl m-7"
+                src={item.images}
+                alt={item.title}
+                width={200}
+                height={200}
+              />
+
+              <span className="fle">
+                Price:$
+                {item.price}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
